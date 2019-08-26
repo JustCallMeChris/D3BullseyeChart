46cb9685261bbe188d259d138bf7b721d25e2c5b
Chris Long <chris@long.works>
2019-08-26 18:58:37 +0200

    Add "working" and "zoomable" Bullseye Chart.
    Zoomable Bulleye Chart.
    Only zoomable in chart areas.
    There is still a stupid way static implemented for donut subplot creation.

--- bullseye_chart.js
+++ bullseye_chart.js
@@ -1,85 +1,106 @@
 // margin
-const margin = {top: 20, right: 20, bottom: 20, left: 20},
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
     width = 500 - margin.right - margin.left,
     height = 500 - margin.top - margin.bottom,
-    radius = width / 2;
+    radius = width/2;
 
 // color range
-const color = d3.scaleOrdinal()
+var color = d3.scaleOrdinal()
     .range(["#BBDEFB", "#90CAF9", "#64B5F6", "#42A5F5", "#2196F3", "#1E88E5", "#1976D2"]);
 
-let zoom = d3.zoom()
-    .scaleExtent([1, 100])
-    .translateExtent([[150, 150], [150, 150]])
-    .on('zoom', zoomFn);
+// pie chart arc. Need to create arcs before generating pie
+// donut chart arc
+var arc2 = d3.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
 
-function zoomFn() {
-    d3.select('svg').select('g')
-        .style('transform', 'scale(' + d3.event.transform.k + ')');
-}
+// arc for the labels position
+var labelArc = d3.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+var arc3 = d3.arc()
+    .outerRadius(radius - 80)
+    .innerRadius(radius - 140);
+
+// arc for the labels position
+var labelArc2 = d3.arc()
+    .outerRadius(radius - 120)
+    .innerRadius(radius - 120);
+
 
 // generate pie chart and donut chart
-const pie = d3.pie()
+var pie = d3.pie()
     .sort(null)
-    .value(function (d) {
-        return d.min;
-    });
+    .value(function(d) { return d.min; });
+
 
 // define the svg donut chart
-const svgContainer = d3.select("body").append("svg")
+var svg = d3.select("body").append("svg")
+    .style("transform-origin", "50% 50% 0")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 
+
 // import data
-d3.csv("data.csv", function (error, data) {
+d3.csv("data.csv", function(error, data) {
     if (error) throw error;
 
-    for (let i = 1; i < 3; i++) {
+    // "g element is a container used to group other SVG elements"
+    var g = svg.selectAll(".arc2")
+        .data(pie(data.splice(0,6)))
+        .enter().append("g")
+        .attr("class", "arc2");
 
-        const pieChart = d3.select("g")
-            .append("pieChart")
-            .attr("width", width)
-            .attr("height", height)
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        let arc = d3.arc()
-            .outerRadius(radius/2*i - 10)
-            .innerRadius(radius/2*i - 70);
-
-        // arc for the labels position
-        let labelArc = d3.arc()
-            .outerRadius(radius/2*i - 40)
-            .innerRadius(radius/2*i - 40);
-
-        // "g element is a container used to pieChart other SVG elements"
-        let arcSlice = pieChart.selectAll(".arc")
-            .data(pie(data.splice(0, data.length / 2)))
-            .enter().append("arcSlice")
-            .attr("class", "arc");
-
-        // append path
-        arcSlice.append("path")
-            .attr("d", arc)
-            .style("fill", function (d) {
-                return color(d.data.name);
-            })
-            .transition()
-            .ease(d3.easeLinear);
-
-        // append text
-        arcSlice.append("text")
-            .transition()
-            .ease(d3.easeLinear)
-            .duration(0)
-            .attr("transform", function (d) {
-                return "translate(" + labelArc.centroid(d) + ")";
-            })
-            .attr("dy", ".35em")
-            .text(function (d) {
-                return d.data.name;
-            });
+    function zoomed() {
+        g.style('transform', 'scale(' + d3.event.transform.k + ')');
     }
+
+    svg.call(d3.zoom().scaleExtent([1, Infinity])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed));
+
+    // append path
+    g.append("path")
+        .attr("d", arc2)
+        .style("fill", function(d) { return color(d.data.name); })
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(500);
+
+    svg.selectAll(".arc3")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc3");
+
+    // append path
+    g.append("path")
+        .attr("d", arc3)
+        .style("fill", function(d) { return color(d.data.name); })
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(500);
+
+    // append text
+    g.append("text")
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(0)
+        .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) { return d.data.name; });
+
+    // append text
+    g.append("text")
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(0)
+        .attr("transform", function(d) { return "translate(" + labelArc2.centroid(d) + ")"; })
+        .attr("dy", ".25em")
+        .text(function(d) { return d.data.name; });
+
 });
